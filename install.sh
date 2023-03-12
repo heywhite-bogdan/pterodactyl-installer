43cddd093febd283ae02ed404fcb38da307a3154
heywhite-bogdan <127649289+heywhite-bogdan@users.noreply.github.com>
2023-03-12 08:32:15 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+###############################################################################
+#                                                                             #
+# Project 'Pterodactyc Installer' By 'HeyWhite'                               #
+#                                                                             #
+# Copyright (C) 2022-2023, HeyWhite, <heywhite@hostverse.eu>                  #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU General Public License for more details.                              #
+#                                                                             #
+#   You should have received a copy of the GNU General Public License         #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+#                                                                             #
+# This script is not associated with the official Pterodactyl Project.        #
+#                                                                             #
+# https://github.com/heywhite-bogdan/pterodactyl/installer                    #
+# https://paypal.com/ninenodes, support me with money                         #
+#                                                                             #
+###############################################################################
+
+export GITHUB_SOURCE="v0.12.3"
+export SCRIPT_RELEASE="v0.12.3"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/heywhite-bogdan/pterodactyl/installer"
+
+LOG_PATH="/var/log/pterodactyl-installer.log"
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+# Always remove lib.sh, before downloading it
+rm -rf /tmp/lib.sh
+curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/"$GITHUB_SOURCE"/lib/lib.sh
+# shellcheck source=lib/lib.sh
+source /tmp/lib.sh
+
+execute() {
+  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+
+  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
+  update_lib_source
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
+
+  if [[ -n $2 ]]; then
+    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    read -r CONFIRM
+    if [[ "$CONFIRM" =~ [Yy] ]]; then
+      execute "$2"
+    else
+      error "Installation of $2 aborted."
+      exit 1
+    fi
+  fi
+}
+
+welcome ""
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install Panel"
+    "Install Wings"
+    "Install Panel & Wings"
+
+    "Unninstal Panel"
+    "Unninstal Wings"
+    "Unninstal Panel & Wings"
+  )
+
+  actions=(
+    "panel"
+    "wings"
+    "panel;wings"
+
+    "nopanel"
+    "nowings"
+    "nopanel;wings"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
+
+# Remove lib.sh, so next time the script is run the, newest version is downloaded.
+rm -rf /tmp/lib.sh
+

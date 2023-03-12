7338d8a603a8ec5c79d92081bbeb77f8e4e8da9f
heywhite-bogdan <127649289+heywhite-bogdan@users.noreply.github.com>
2023-03-12 08:52:50 +0200

    Create lib.sh

---
+++ lib/lib.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+######################################################################################
+#                                                                                    #
+# Project 'pterodactyl-installer'                                                    #
+#                                                                                    #
+# Copyright (C) 2018 - 2023, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
+#                                                                                    #
+#   This program is free software: you can redistribute it and/or modify             #
+#   it under the terms of the GNU General Public License as published by             #
+#   the Free Software Foundation, either version 3 of the License, or                #
+#   (at your option) any later version.                                              #
+#                                                                                    #
+#   This program is distributed in the hope that it will be useful,                  #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
+#   GNU General Public License for more details.                                     #
+#                                                                                    #
+#   You should have received a copy of the GNU General Public License                #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
+#                                                                                    #
+# https://github.com/pterodactyl-installer/pterodactyl-installer/blob/master/LICENSE #
+#                                                                                    #
+# This script is not associated with the official Pterodactyl Project.               #
+# https://github.com/pterodactyl-installer/pterodactyl-installer                     #
+#                                                                                    #
+######################################################################################
+
+# ------------------ Variables ----------------- #
+
+# Versioning
+export GITHUB_SOURCE=${GITHUB_SOURCE:-master}
+export SCRIPT_RELEASE=${SCRIPT_RELEASE:-canary}
+
+# Pterodactyl versions
+export PTERODACTYL_PANEL_VERSION=""
+export PTERODACTYL_WINGS_VERSION=""
+
+# Path (export everything that is possible, doesn't matter that it exists already)
+export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+
+# OS
+export OS=""
+export OS_VER_MAJOR=""
+export CPU_ARCHITECTURE=""
+export ARCH=""
+export SUPPORTED=false
+
+# download URLs
+export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
+export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
+export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
+export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
+
+# Colors
+COLOR_YELLOW='\033[1;33m'
+COLOR_GREEN='\033[0;32m'
